000a7bfefc60336019e8631fd8b2bdb52fc94354
Jean-Baptiste Blandino, o.p. <outils.apostoliques.op@gmail.com>
2025-12-23 22:23:19 +0100

    hymnes du temps pour les complies

---
+++ js/complies_temporal-v0.js
@@ -0,0 +1,70 @@
+const hymnes = {
+    "to": {
+        "Avant la fin de la lumière": "Avant la fin de la lumière,<br />Nous te prions, Dieu créateur,<br />Pour que, fidèle à ta bonté,<br />Tu nous protèges, tu nous gardes.<br /><br />Que loin de nous s’enfuient les songes,<br />Et les angoisses de la nuit.<br />Préserve-nous de l’ennemi :<br />Que ton amour sans fin nous garde.<br /><br />Exauce-nous, Dieu, notre Père,<br />Par Jésus Christ, notre Seigneur,<br />Dans l’unité du Saint-Esprit,<br />Régnant sans fin dans tous les siècles. Amen.",
+        "Te Lucis ante terminum": "Te lucis ante terminum,<br />Rerum Creator, poscimus<br />Ut pro tua clementia<br />Sis præsul et custodia.<br /><br />Procul recedant somnia,<br />Et noctium phantasmata ;<br />Hostemque nostrum comprime<br />Nepolluantur corpora.<br /><br />Præsta, Pater piissime,<br />Patrique compar Unice,<br />Cum Spiritu Paraclito<br />Regnans per omne sæculum. Amen.",
+    },
+    "avent": {
+        "Avant la fin de la lumière": "Avant la fin de la lumière,<br />Nous te prions, Dieu créateur,<br />Pour que, fidèle à ta bonté,<br />Tu nous protèges, tu nous gardes.<br /><br />Que loin de nous s’enfuient les songes,<br />Et les angoisses de la nuit.<br />Préserve-nous de l’ennemi :<br />Que ton amour sans fin nous garde.<br /><br />Exauce-nous, Dieu, notre Père,<br />Par Jésus Christ, notre Seigneur,<br />Dans l’unité du Saint-Esprit,<br />Régnant sans fin dans tous les siècles. Amen.",
+        "Te Lucis ante terminum": "Te lucis ante terminum,<br />Rerum Creator, poscimus<br />Ut pro tua clementia<br />Sis præsul et custodia.<br /><br />Procul recedant somnia,<br />Et noctium phantasmata ;<br />Hostemque nostrum comprime<br />Nepolluantur corpora.<br /><br />Præsta, Pater piissime,<br />Patrique compar Unice,<br />Cum Spiritu Paraclito<br />Regnans per omne sæculum. Amen.",
+    },
+    "noel": {
+        "Avant la fin de la lumière": "Avant la fin de la lumière,<br />Nous te prions, Dieu créateur,<br />Pour que, fidèle à ta bonté,<br />Tu nous protèges, tu nous gardes.<br /><br />Que loin de nous s’enfuient les songes,<br />Et les angoisses de la nuit.<br />Préserve-nous de l’ennemi :<br />Que ton amour sans fin nous garde.<br /><br />Exauce-nous, Dieu, notre Père,<br />Par Jésus Christ, notre Seigneur,<br />Dans l’unité du Saint-Esprit,<br />Régnant sans fin dans tous les siècles. Amen.",
+        "Te Lucis ante terminum": "Te lucis ante terminum,<br />Rerum Creator, poscimus<br />Ut pro tua clementia<br />Sis præsul et custodia.<br /><br />Procul recedant somnia,<br />Et noctium phantasmata ;<br />Hostemque nostrum comprime<br />Nepolluantur corpora.<br /><br />Præsta, Pater piissime,<br />Patrique compar Unice,<br />Cum Spiritu Paraclito<br />Regnans per omne sæculum. Amen.",
+    },
+    "epiphanie": {
+        "Avant la fin de la lumière": "Avant la fin de la lumière,<br />Nous te prions, Dieu créateur,<br />Pour que, fidèle à ta bonté,<br />Tu nous protèges, tu nous gardes.<br /><br />Que loin de nous s’enfuient les songes,<br />Et les angoisses de la nuit.<br />Préserve-nous de l’ennemi :<br />Que ton amour sans fin nous garde.<br /><br />Exauce-nous, Dieu, notre Père,<br />Par Jésus Christ, notre Seigneur,<br />Dans l’unité du Saint-Esprit,<br />Régnant sans fin dans tous les siècles. Amen.",
+        "Te Lucis ante terminum": "Te lucis ante terminum,<br />Rerum Creator, poscimus<br />Ut pro tua clementia<br />Sis præsul et custodia.<br /><br />Procul recedant somnia,<br />Et noctium phantasmata ;<br />Hostemque nostrum comprime<br />Nepolluantur corpora.<br /><br />Præsta, Pater piissime,<br />Patrique compar Unice,<br />Cum Spiritu Paraclito<br />Regnans per omne sæculum. Amen.",
+    },
+    "careme": {
+        "Christe qui lux es": "Christe qui lux es et dies,<br />Noctis ténebras détegis,<br />Lucísque lumen créderis,<br />Lumen beátum prædicans.<br /><br />Precámur, sancte Dómine,<br />Defénde nos in hac nocte :<br />Sit nobis in te réquies,<br />Quiétam noctem tríbue.<br /><br />Ne gravis somnus írruat,<br />Nec hostis nos surrípiat :<br />Nec caro illi conséntiens<br />Nos tibi reos státuat.<br /><br />Oculi somnum cápiant,<br />Cor ad te semper vígilet :<br />Déxtera tua prótegat<br />Fámulos qui te díligunt.<br /><br />Defénsor noster, áspice,<br />Insidiántes réprime :<br />Gubérna tuos fámulos<br />Quos sánguine mercátus es.<br /><br />Meménto nostri, Dómine,<br />In gravi isto córpore :<br />Qui es defénsor ánimæ,<br />Adesto nobis, Dómine.<br /><br />Deo Patri sit glória,<br />Ejusque soli Fílio,<br />Cum Spirítu Paráclito,<br />Et nunc et in perpétuum. Amen.",
+    },
+    "passion": {
+        "Christe qui lux es": "Christe qui lux es et dies,<br />Noctis ténebras détegis,<br />Lucísque lumen créderis,<br />Lumen beátum prædicans.<br /><br />Precámur, sancte Dómine,<br />Defénde nos in hac nocte :<br />Sit nobis in te réquies,<br />Quiétam noctem tríbue.<br /><br />Ne gravis somnus írruat,<br />Nec hostis nos surrípiat :<br />Nec caro illi conséntiens<br />Nos tibi reos státuat.<br /><br />Oculi somnum cápiant,<br />Cor ad te semper vígilet :<br />Déxtera tua prótegat<br />Fámulos qui te díligunt.<br /><br />Defénsor noster, áspice,<br />Insidiántes réprime :<br />Gubérna tuos fámulos<br />Quos sánguine mercátus es.<br /><br />Meménto nostri, Dómine,<br />In gravi isto córpore :<br />Qui es defénsor ánimæ,<br />Adesto nobis, Dómine.<br /><br />Deo Patri sit glória,<br />Ejusque soli Fílio,<br />Cum Spirítu Paráclito,<br />Et nunc et in perpétuum. Amen.",
+    },
+    "pascal": {
+        "O Jésus, notre rédempteur": "O Jésus, notre rédempteur,<br />amour qui comble nos désirs,<br />Dieu créateur de l’univers,<br />Verbe fait chair aux derniers temps.<br /><br />Quel amour a pu te pousser<br />à te charger de nos péchés<br />innocent, à subir la mort<br />pour nous arracher à la mort.<br /><br />Des enfers tu forces les portes,<br />des captifs tu brises les chaînes ;<br />vainqueur par un noble triomphe,<br />tu sièges à la droite du Père.<br /><br />Que ton indulgence t’entraîne<br />à réparer tous nos malheurs,<br />à nous placer devant ta face<br />dans ta lumière bienheureuse.<br /><br />Ô Créateur, nous te prions<br />dans la joie des fêtes pascales,<br />que ton peuple soit défendu<br />de l’ennemi qui le menace.<br /><br />Gloire à toi, Seigneur Jésus-Christ,<br />ressuscité d’entre les morts,<br />avec le Père et dans l’Esprit,<br />aux trois la même adoration.<br />Amen. Alléluia.<br />",
+        "Jesu, nostra redémptio": "Jesu, nostra redémptio,<br />Amor et desidérium,<br />Deus Creátor ómnium,<br />Homo in fine témporum.<br /><br />Quæ te vicit cleméntia,<br />Ut ferres nostra crímina,<br />Crudélem mortem pátiens,<br />Ut nos a morte tólleres ?<br /><br />Inférni claustra pénetrans,<br />Tuos captívos rédimens,<br />Victor triúmpho nóbili<br />Ad dextram Patris résidens.<br /><br />Ipsa te cogat píetas,<br />Ut mala nostra súperes<br />Parcéndo, et voti cómpotes<br />Nos tuo vultu sáties.<br /><br />Quaesumus Auctor ómnium,<br />in hoc páschali gáudio,<br />ab omni mortis ímpetu<br />tuum défende pópulum.<br /><br />Glória tibi Dómine,<br />qui surrexísti a mórtuis,<br />cum Patre et Sancto Spíritu,<br />in sempitérna sæcula. Amen.<br /><br />Entre l’Ascension et la Pentecôte<br /><br />Tu esto nostrum gáudium,<br />Qui es futúrus præmium :<br />Sit nostra in te glória<br />Per cuncta semper sæcula.<br /><br /> Glória tibi Dómine,<br />qui scanda supra sídera,<br />cum Patre et Sancto Spíritu,<br />in sempitérna sæcula. Amen.<br /><br />À la Pentecôte<br /><br />Dudum sacráta péctora<br />tua réplesti grátia<br />dimítte nostra peccámina,<br />et da quiéta témpora.<br /><br />Sit laus Patri cum Fílio,<br />Spíritu simul Paráclito,<br />nobísque mittat Fílius<br />charísma Sancti Spíritus. Amen.<br /><br /><br /><br />Aux fêtes et solennités de la Vierge Marie<br /><br />María, Mater grátiæ,<br />Dulcis parens cleméntiæ,<br />Tu nos ab hoste prótege,<br />Et mortis hora súscipe.<br /><br />Glória tibi Dómine,<br />qui natus es de Vírgine,<br />cum Patre et Sancto Spíritu,<br />in sempitérna sæcula. Amen.",
+    }
+}
+
+function liste_hymnescomplies(periode){
+  return Object.keys(hymnescomplies[periode]);
+}
+
+function hymne_aelf2bref(titre){
+    var titre_clean = titre.replaceAll("'", "’").trim();
+    if (titre_clean.slice(-1) == "." || titre_clean.slice(-1) == ","){
+        titre_clean = titre_clean.slice(0, -1);
+    }
+    return titre_clean
+}
+
+function hymnecomplies_from_titre(titre){
+  let titre_clean = hymne_aelf2bref(titre);
+  let texte = "";
+    for (var periode of Object.keys(hymnes)){
+        if (hymnescomplies[periode].hasOwnProperty(titre_clean)){
+            texte = hymnescomplies[periode][titre_clean];
+            break;
+        }
+    }
+    return texte;
+}
+
+function hymnecomplies_in_periode(titre, periode){
+    let titre_clean = hymne_aelf2bref(titre);
+    if (hymnescomplies[periode].hasOwnProperty(titre_clean)){
+        return true;
+    }
+    return false;
+}
+
+//export { liste_hymnescomplies, hymne_from_titre };
+
+function main(){
+    console.log(liste_hymnescomplies("avent"));
+    console.log(hymnecomplies_from_titre("Esprit qui planes sur les eaux"));
+    
+}
+
+//main();
